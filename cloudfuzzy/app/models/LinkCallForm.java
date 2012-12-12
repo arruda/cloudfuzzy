b021b130c2cf62f7ace1b9fd93976346da7ca9a5
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-12-12 12:22:04 -0200

    System Design Js- Doing the link painting refs #47

--- cloudfuzzy/app/models/LinkCallForm.java
+++ cloudfuzzy/app/models/LinkCallForm.java
@@ -125,6 +125,100 @@ public class LinkCallForm{
             return rbc;
 
         }
-    }
 
+//        /**
+//         * Returns a variable dot for a given fuzzy system, giving a Variable(Fuzzy) as parameter
+//         * @param sys
+//         * @param var
+//         */
+//        public static VariableDot get(FuzzySystem sys, Variable var){
+//        	VariableDot varDot = null;
+//        	if(var == null) return varDot;
+//        	
+//        	//if is a inner variable means I have to search for it pair's in another rulebasecall
+//        	if(var.isInner()){
+//            	
+//            	xfuzzy.lang.RulebaseCall rulebaseCalls[] = sys.getSpecification().getSystemModule().getRulebaseCalls();
+//            	
+//            	for (int irbc = 0; irbc < rulebaseCalls.length; irbc++) {
+//            		xfuzzy.lang.RulebaseCall rulebaseCall = rulebaseCalls[irbc];
+//            		
+//            		
+//            		  
+//            		  
+//            		  
+//    			}
+//            	
+//        	}
+//        	//If isnot a inner variable, then I can just look for the systems vars
+//        	else{
+//        		if(var.isInner()){
+//            		Variable []sysInputVars = sys.getSpecification().getSystemModule().getInputs();
+//            		for (int isiv = 0; isiv < sysInputVars.length; isiv++) {
+//            			//found it
+//    					if(sysInputVars[isiv] == var){
+//    						varDot = new VariableDot();
+//    						varDot.idSysVar = isiv;
+//    						varDot.kindSysVar = Variable.INPUT;
+//    						varDot.setSysVar(sysInputVars[isiv]);
+//    						return varDot;
+//    					}
+//    				}
+//        		}
+//        		else{
+//            		Variable []sysOutputVars = sys.getSpecification().getSystemModule().getOutputs();
+//            		for (int isov = 0; isov < sysOutputVars.length; isov++) {
+//            			//found it
+//    					if(sysOutputVars[isov] == var){
+//    						varDot = new VariableDot();
+//    						varDot.idSysVar = isov;
+//    						varDot.kindSysVar = Variable.OUTPUT;
+//    						varDot.setSysVar(sysOutputVars[isov]);
+//    						return varDot;
+//    					}
+//    				}
+//        		}
+//        		
+//        		
+//        	}
+//        	
+//        	
+//        	return varDot;
+//        }
+        
+
+    }
+    
+    /**
+     *  Returns all linkcallform for a given sys.
+     *  
+     * @param sys
+     * @return
+     */
+    public static ArrayList<LinkCallForm> getLinks(FuzzySystem sys){
+
+    	ArrayList<LinkCallForm> links = new ArrayList<LinkCallForm>();
+    	xfuzzy.lang.RulebaseCall rulebaseCalls[] = sys.getSpecification().getSystemModule().getRulebaseCalls();
+        
+
+    	for (int irbc = 0; irbc < rulebaseCalls.length; irbc++) {
+    		xfuzzy.lang.RulebaseCall rulebaseCall = rulebaseCalls[irbc];
+    		
+    		for (int ivi = 0; ivi < rulebaseCall.getInputValues().length; ivi++) {
+    			Variable linkVariable =  rulebaseCall.getInputVariables()[ivi];    	
+    			
+    			//There is a connection to here.(this is the destDot)
+				if(!linkVariable.getName().equals("NULL")){
+					VariableDot destDot = new VariableDot();
+					destDot.idRuleBaseCall = irbc;
+					destDot.idBaseVar = ivi;
+					destDot.kindBaseVar = Variable.INPUT;
+				}
+				
+			}
+    		
+		}
+    	return null;
+    }
+    
 }
