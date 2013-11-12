a9d84c51930cb05229066f1a73105042c868f290
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-12 12:41:51 -0200

    adding my manifest file

---
+++ manifests/cf.pp
@@ -0,0 +1,23 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core'
+  ]:
+  ensure  => 'installed',
+}->
+
+class { 'java':
+} ->
+class { 'playframework':
+    version => "2.0.2",
+}
+
