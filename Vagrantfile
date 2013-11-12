41a06432fcb0b4abb04cfccfd201cce53807e0f3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-11-12 13:59:12 -0200

    Now port 9000 on host redirect to 9000 on guess, and set the env vars for cf in manifests/cf.pp Also changed the initial data

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,7 @@ Vagrant.configure("2") do |config|
 
 
   config.vm.network :private_network, ip: "192.168.56.101"
+  config.vm.network :forwarded_port, guest: 9000, host: 9000
 
 
   config.vm.provider :virtualbox do |vb|
--- manifests/cf.pp
+++ manifests/cf.pp
@@ -21,3 +21,14 @@ class { 'playframework':
     version => "2.0.2",
 }
 
+->
+exec { "add_cf_env_vars_CF_XFL_PATH":
+    command => "/bin/echo 'export CF_XFL_PATH=/vagrant/extraFiles/xfls/' >> /home/vagrant/.bashrc",
+    unless    => "cat /home/vagrant/.bashrc | grep -e 'CF_XFL_PATH'",
+}
+
+->
+exec { "add_cf_env_vars_CF_PKG_PATH":
+    command => "/bin/echo 'export CF_PKG_PATH=/vagrant/extraFiles/' >> /home/vagrant/.bashrc",
+    unless    => "cat /home/vagrant/.bashrc | grep -e 'CF_PKG_PATH'",
+}
