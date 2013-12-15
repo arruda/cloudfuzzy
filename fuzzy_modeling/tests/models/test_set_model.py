33125ec8633d40de9d5283e81f4eb99db2e4eb39
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-12-15 17:09:25 -0200

    finishing get_pyfuzzy tests of SetModel

--- fuzzy_modeling/tests/models/test_set_model.py
+++ fuzzy_modeling/tests/models/test_set_model.py
@@ -11,6 +11,14 @@ from fuzzy_modeling.models.sets import SetModel
 from fuzzy.set.Set import Set
 from fuzzy.set.Polygon import Polygon
 from fuzzy.set.Triangle import Triangle
+from fuzzy.set.Singleton import Singleton
+from fuzzy.set.Trapez import Trapez
+from fuzzy.set.Function import Function
+from fuzzy.set.SFunction import SFunction
+from fuzzy.set.ZFunction import ZFunction
+from fuzzy.set.PiFunction import PiFunction
+
+
 
 
 class SetModelTest(TestCase, ResetMock):
@@ -125,3 +133,191 @@ class SetModelTest(TestCase, ResetMock):
         self.assertEquals(pyfuzzy_set_expected.beta, new_pyfuzzy_set.beta)
         self.assertEquals(pyfuzzy_set_expected.y_max, new_pyfuzzy_set.y_max)
         self.assertEquals(pyfuzzy_set_expected.y_min, new_pyfuzzy_set.y_min)
+
+    def test_set_get_pyfuzzy_for_singleton_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the Singleton type "
+
+        new_set = self._mock_setModel('fuzzy.set.Singleton.Singleton')
+
+        x = 1.2
+
+
+        self.parameters_mock = [
+                self._parameters_mock(name="x", value=x),
+            ]
+
+        # mocking parameters (queryset)
+        parameters_queryset = mock.Mock()
+        parameters_queryset.all = lambda : self.parameters_mock
+        self.set_pre_mock(SetModel,'parameters')
+        SetModel.parameters = parameters_queryset
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = Singleton(x=x)
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+        # have the same args
+        self.assertEquals(pyfuzzy_set_expected.x, new_pyfuzzy_set.x)
+
+    def test_set_get_pyfuzzy_for_trapez_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the Trapez type "
+
+        new_set = self._mock_setModel('fuzzy.set.Trapez.Trapez')
+
+        m1 = 1.2
+        m2 = 1.3
+        alpha = 2.3
+        beta = 3.4
+        y_max = 4.5
+        y_min = 5.4
+
+
+        self.parameters_mock = [
+                self._parameters_mock(name="m1", value=m1),
+                self._parameters_mock(name="m2", value=m2),
+                self._parameters_mock(name="alpha", value=alpha),
+                self._parameters_mock(name="beta", value=beta),
+                self._parameters_mock(name="y_max", value=y_max),
+                self._parameters_mock(name="y_min", value=y_min)
+            ]
+
+        # mocking parameters (queryset)
+        parameters_queryset = mock.Mock()
+        parameters_queryset.all = lambda : self.parameters_mock
+        self.set_pre_mock(SetModel,'parameters')
+        SetModel.parameters = parameters_queryset
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = Trapez(m1 = m1, m2 = m2, alpha = alpha, beta = beta, y_max = y_max, y_min = y_min)
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+        # have the same args
+        self.assertEquals(pyfuzzy_set_expected.m1, new_pyfuzzy_set.m1)
+        self.assertEquals(pyfuzzy_set_expected.m2, new_pyfuzzy_set.m2)
+        self.assertEquals(pyfuzzy_set_expected.alpha, new_pyfuzzy_set.alpha)
+        self.assertEquals(pyfuzzy_set_expected.beta, new_pyfuzzy_set.beta)
+        self.assertEquals(pyfuzzy_set_expected.y_max, new_pyfuzzy_set.y_max)
+        self.assertEquals(pyfuzzy_set_expected.y_min, new_pyfuzzy_set.y_min)
+
+    def test_set_get_pyfuzzy_for_function_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the Function type "
+
+        new_set = self._mock_setModel('fuzzy.set.Function.Function')
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = Function()
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+    def test_set_get_pyfuzzy_for_sfunction_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the SFunction type "
+
+        new_set = self._mock_setModel('fuzzy.set.SFunction.SFunction')
+
+        a = 1.2
+        delta = 2.3
+
+
+        self.parameters_mock = [
+                self._parameters_mock(name="a", value=a),
+                self._parameters_mock(name="delta", value=delta),
+            ]
+
+        # mocking parameters (queryset)
+        parameters_queryset = mock.Mock()
+        parameters_queryset.all = lambda : self.parameters_mock
+        self.set_pre_mock(SetModel,'parameters')
+        SetModel.parameters = parameters_queryset
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = SFunction(a = a, delta = delta)
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+        # have the same args
+        self.assertEquals(pyfuzzy_set_expected.a, new_pyfuzzy_set.a)
+        self.assertEquals(pyfuzzy_set_expected.delta, new_pyfuzzy_set.delta)
+
+    def test_set_get_pyfuzzy_for_zfunction_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the ZFunction type "
+
+        new_set = self._mock_setModel('fuzzy.set.ZFunction.ZFunction')
+
+        a = 1.2
+        delta = 2.3
+
+
+        self.parameters_mock = [
+                self._parameters_mock(name="a", value=a),
+                self._parameters_mock(name="delta", value=delta),
+            ]
+
+        # mocking parameters (queryset)
+        parameters_queryset = mock.Mock()
+        parameters_queryset.all = lambda : self.parameters_mock
+        self.set_pre_mock(SetModel,'parameters')
+        SetModel.parameters = parameters_queryset
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = ZFunction(a = a, delta = delta)
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+        # have the same args
+        self.assertEquals(pyfuzzy_set_expected.a, new_pyfuzzy_set.a)
+        self.assertEquals(pyfuzzy_set_expected.delta, new_pyfuzzy_set.delta)
+
+    def test_set_get_pyfuzzy_for_pifunction_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the PiFunction type "
+
+        new_set = self._mock_setModel('fuzzy.set.PiFunction.PiFunction')
+
+        a = 1.2
+        delta = 2.3
+
+
+        self.parameters_mock = [
+                self._parameters_mock(name="a", value=a),
+                self._parameters_mock(name="delta", value=delta),
+            ]
+
+        # mocking parameters (queryset)
+        parameters_queryset = mock.Mock()
+        parameters_queryset.all = lambda : self.parameters_mock
+        self.set_pre_mock(SetModel,'parameters')
+        SetModel.parameters = parameters_queryset
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = PiFunction(a = a, delta = delta)
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+        # have the same args
+        self.assertEquals(pyfuzzy_set_expected.a, new_pyfuzzy_set.a)
+        self.assertEquals(pyfuzzy_set_expected.delta, new_pyfuzzy_set.delta)
