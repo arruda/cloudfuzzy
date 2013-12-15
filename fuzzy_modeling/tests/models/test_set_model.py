984af135312052a316db38413ae6f43d08dfa98e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-12-15 16:53:08 -0200

    testing from_pyfuzzy in setmodel (for triangle)

--- fuzzy_modeling/tests/models/test_set_model.py
+++ fuzzy_modeling/tests/models/test_set_model.py
@@ -10,6 +10,7 @@ from fuzzy_modeling.models.sets import SetModel
 
 from fuzzy.set.Set import Set
 from fuzzy.set.Polygon import Polygon
+from fuzzy.set.Triangle import Triangle
 
 
 class SetModelTest(TestCase, ResetMock):
@@ -44,17 +45,6 @@ class SetModelTest(TestCase, ResetMock):
         " shoud return the correct corresponding pyfuzzy object for the Set type "
 
         new_set = self._mock_setModel('fuzzy.set.Set.Set')
-
-        # self.parameters_mock = [
-        #         self._parameters_mock(name="x", value="123")
-        #     ]
-
-        # mocking parameters (queryset)
-        # parameters_queryset = mock.Mock()
-        # parameters_queryset.all = lambda : self.parameters_mock
-        # self.set_pre_mock(SetModel,'parameters')
-        # SetModel.parameters = parameters_queryset
-
 
         new_pyfuzzy_set = new_set.get_pyfuzzy()
 
@@ -93,3 +83,45 @@ class SetModelTest(TestCase, ResetMock):
 
         # have the same points
         self.assertEquals(pyfuzzy_set_expected.points, new_pyfuzzy_set.points)
+
+    def test_set_get_pyfuzzy_for_triangle_type(self):
+        " shoud return the correct corresponding pyfuzzy object for the Triangle type "
+
+        new_set = self._mock_setModel('fuzzy.set.Triangle.Triangle')
+
+        m = 1.2
+        alpha = 2.3
+        beta = 3.4
+        y_max = 4.5
+        y_min = 5.4
+
+
+        self.parameters_mock = [
+                self._parameters_mock(name="m", value=m),
+                self._parameters_mock(name="alpha", value=alpha),
+                self._parameters_mock(name="beta", value=beta),
+                self._parameters_mock(name="y_max", value=y_max),
+                self._parameters_mock(name="y_min", value=y_min)
+            ]
+
+        # mocking parameters (queryset)
+        parameters_queryset = mock.Mock()
+        parameters_queryset.all = lambda : self.parameters_mock
+        self.set_pre_mock(SetModel,'parameters')
+        SetModel.parameters = parameters_queryset
+
+
+        new_pyfuzzy_set = new_set.get_pyfuzzy()
+
+        # the expected pyfuzzy system
+        pyfuzzy_set_expected = Triangle(m = m, alpha = alpha, beta = beta, y_max = y_max, y_min = y_min)
+
+        # are from the same class
+        self.assertEquals(type(pyfuzzy_set_expected), type(new_pyfuzzy_set))
+
+        # have the same args
+        self.assertEquals(pyfuzzy_set_expected.m, new_pyfuzzy_set.m)
+        self.assertEquals(pyfuzzy_set_expected.alpha, new_pyfuzzy_set.alpha)
+        self.assertEquals(pyfuzzy_set_expected.beta, new_pyfuzzy_set.beta)
+        self.assertEquals(pyfuzzy_set_expected.y_max, new_pyfuzzy_set.y_max)
+        self.assertEquals(pyfuzzy_set_expected.y_min, new_pyfuzzy_set.y_min)
