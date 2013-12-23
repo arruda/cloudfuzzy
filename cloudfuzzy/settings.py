1500d3c6a1c6c07b8bfc3c23ede1b69afc9437c3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-12-23 02:45:51 -0200

    adding html5 boilerplate app

--- cloudfuzzy/settings.py
+++ cloudfuzzy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_extensions',
     'mptt',
+    'h5bp',
     'fuzzy_modeling',
 )
 
--- cloudfuzzy/urls.py
+++ cloudfuzzy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cloudfuzzy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index.html'),
     url(r'^admin/', include(admin.site.urls)),
 )
