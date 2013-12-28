14224ed9c815c79631b40105d25e43364eafffba
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-12-28 01:11:35 -0200

    doind a jquery template

--- cloudfuzzy/urls.py
+++ cloudfuzzy/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cloudfuzzy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', TemplateView.as_view(template_name="base.html"), name='index.html'),
+    url(r'^$', TemplateView.as_view(template_name="base.html"), name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
