67b8ab1886dacd2a8256860c5805508e17fe2955
Bruno Barcellos <bbarcellosf@gmail.com>
2014-01-28 16:13:15 -0200

    Include the text of the license in the head of java files. refs #56

--- cloudfuzzy/app/controllers/Types.java
+++ cloudfuzzy/app/controllers/Types.java
@@ -1,3 +1,36 @@
+ 
+	/*
+	*
+    * (c) 2013, 2014 INT - National Institute of Technology
+	*
+    * CloudFuzzy's team: 
+	*	Dayse Mourao Arruda, Felipe Arruda Pontes, Bruno Barcellos Farias de Oliveira.
+	*
+    * The authors may be contacted by the email address: dayse.arruda@int.gov.br
+	*
+	* This file is part of CloudFuzzy - version Beta - 2014/January.
+	*
+    * We apply XFuzzy 3.0 as fuzzy engine to generate and evaluate alternative plans.
+    * XFuzzy is a free design environment for fuzzy logic based systems distributed by IMSE-CNM.
+	* If you need more information about the XFuzzy 3.0, please go to project's site:
+	* http://www2.imse-cnm.csic.es/Xfuzzy/
+    *                    
+	* CloudFuzzy is free software: you can redistribute it and/or modify
+	* it under the terms of the GNU General Public License as published by
+	* the Free Software Foundation, either version 3 of the License, or
+	* (at your option) any later version.
+	*
+	* CloudFuzzy is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	* GNU General Public License for more details.
+	*
+	* You should have received a copy of the GNU General Public License
+	* along with CloudFuzzy. If not, see <http://www.gnu.org/license/>.
+    */
+        
+           
+     
 package controllers;
 
 import java.util.Map;
