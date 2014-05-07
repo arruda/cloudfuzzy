c9856be1d37718667c9fcf089f9db2ecad1139fc
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-05-07 12:24:58 -0300

    removing wrong app

--- cloudfuzzy/settings.py
+++ cloudfuzzy/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'rest_framework',
     'mptt',
     'django_h5bp',
