16176170bab21342ad4b537c4797d0a35072f819
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-05-07 12:30:47 -0300

    print on heroku

--- cloudfuzzy/settings.py
+++ cloudfuzzy/settings.py
@@ -106,6 +106,7 @@ LOGIN_REDIRECT_URL = '/'
 
 ON_HEROKU = 'DATABASE_URL' in os.environ
 if ON_HEROKU:
+    print "on heroku"
     import dj_database_url
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
