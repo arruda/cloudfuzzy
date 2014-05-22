07b1aad5323de2859115d17d0e1120941bb9d660
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-05-21 21:14:39 -0300

    super test pack xD

--- fuzzy_modeling/tests/models/test_system_model.py
+++ fuzzy_modeling/tests/models/test_system_model.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import inspect
+
 import mock
 
 from django.test import TestCase
@@ -331,7 +333,195 @@ class SystemModelTest(TestCase, ResetMock):
         'a' : 0.0 #: acceleration [m/s²]
         }
 
-        pyfuzzy_system_expected.calculate(i_dict1,output_dict1)
-        new_pyfuzzy_system.calculate(i_dict2,output_dict2)
 
-        self.assertEquals(output_dict1['a'], output_dict2['a'])
+        pyfuzzy_system_expected.fuzzify(i_dict1)
+        pyfuzzy_system_expected.inference()
+        # pyfuzzy_system_expected.defuzzify(output_dict1)
+
+        new_pyfuzzy_system.fuzzify(i_dict2)
+        new_pyfuzzy_system.inference()
+        # new_pyfuzzy_system.defuzzify(output_dict2)
+
+        for var_name, var in pyfuzzy_system_expected.variables.items():
+            new_var = new_pyfuzzy_system.variables[var_name]
+
+            self.assertIsInstance(new_var, var.__class__)
+            self.assertEquals(new_var.description, var.description)
+            self.assertEquals(new_var.min, var.min)
+            self.assertEquals(new_var.max, var.max)
+            self.assertEquals(new_var.unit, var.unit)
+
+            for adj_name, adj in var.adjectives.items():
+                new_adj = var.adjectives[adj_name]
+                self._test_adj(adj, new_adj)
+
+            #: is input
+            if hasattr(var, 'fuzzify'):
+                self._test_fuzzify(var.fuzzify, new_var.fuzzify)
+            #: output
+            else:
+                self._test_defuzzify(var.defuzzify, new_var.defuzzify)
+
+            var_value = var.getValue()
+            new_var_value = new_var.getValue()
+            # if var_value != new_var_value:
+            #     import pdb; pdb.set_trace()
+
+            self.assertEquals(new_var_value, var_value)
+
+        for rule_name, rule in pyfuzzy_system_expected.rules.items():
+            new_rule = new_pyfuzzy_system.rules[rule_name]
+            self._test_rule(rule, new_rule)
+
+        # pyfuzzy_system_expected.calculate(i_dict1, output_dict1)
+        # new_pyfuzzy_system.calculate(i_dict2, output_dict2)
+
+        # self.assertEquals(output_dict1['a'], output_dict2['a'])
+
+    def _test_adj(self, adj, new_adj):
+        " test only a given adjective "
+        self.assertIsInstance(new_adj, adj.__class__)
+        self._test_set(adj.set, new_adj.set)
+        if adj.COM is not None and new_adj.COM is not None:
+            self._test_norm(adj.COM, new_adj.COM)
+
+        membership = adj.getMembership()
+        new_membership = new_adj.getMembership()
+        if membership != new_membership:
+            print '>>' + adj.name
+            # import pdb; pdb.set_trace()
+
+        self.assertEquals(
+            membership,
+            new_membership,
+            msg="%s != %s in %s" % (membership, new_membership, adj)
+        )
+
+    def _test_set(self, set, new_set):
+        " test only a given set "
+        self.assertIsInstance(new_set, set.__class__)
+
+        params = []
+        try:
+            for arg in inspect.getargspec(set.__init__).args:
+                if arg != 'self':
+                    params.append(arg)
+        # will raise this exception when the given type don't implement a __init__ function
+        # (never overrided the object.__init__)
+        except TypeError:
+            pass
+
+        for param_name in params:
+            arg = getattr(set, param_name)
+            new_arg = getattr(new_set, param_name)
+            print "points>>>", arg
+            print "new_points>>>", new_arg
+            self.assertEquals(new_arg, arg)
+
+        cog = None
+        new_cog = None
+
+        try:
+            cog = set.getCOG()
+        except:
+            self.assertRaises(Exception, new_set.getCOG)
+        else:
+            new_cog = new_set.getCOG()
+            self.assertEquals(new_cog, cog)
+        self.assertEquals(new_set.points, set.points)
+
+    def _test_norm(self, norm, new_norm):
+        " test only a given norm "
+        self.assertIsInstance(new_norm, norm.__class__)
+
+        params = []
+        try:
+            for arg in inspect.getargspec(norm.__init__).args:
+                if arg != 'self':
+                    params.append(arg)
+        # will raise this exception when the given type don't implement a __init__ function
+        # (never overrided the object.__init__)
+        except TypeError:
+            pass
+
+        for param_name in params:
+            arg = getattr(norm, param_name)
+            new_arg = getattr(new_norm, param_name)
+            self.assertEquals(new_arg, arg)
+
+        self.assertEquals(new_norm.UNKNOWN, norm.UNKNOWN)
+        self.assertEquals(new_norm.T_NORM, norm.T_NORM)
+        self.assertEquals(new_norm.S_NORM, norm.S_NORM)
+
+    def _test_fuzzify(self, fuzzify, new_fuzzify):
+        " test only a given fuzzify "
+        self.assertIsInstance(new_fuzzify, fuzzify.__class__)
+
+    def _test_defuzzify(self, defuzzify, new_defuzzify):
+        " test only a given fuzzify "
+        self.assertIsInstance(new_defuzzify, defuzzify.__class__)
+
+        params = []
+        try:
+            for arg in inspect.getargspec(defuzzify.__init__).args:
+                if arg != 'self' and arg != 'INF' and arg != 'ACC':
+                    params.append(arg)
+        # will raise this exception when the given type don't implement a __init__ function
+        # (never overrided the object.__init__)
+        except TypeError:
+            pass
+
+        for param_name in params:
+            arg = getattr(defuzzify, param_name)
+            new_arg = getattr(new_defuzzify, param_name)
+            self.assertEquals(new_arg, arg)
+
+        self._test_norm(defuzzify.INF, new_defuzzify.INF)
+        self._test_norm(defuzzify._INF, new_defuzzify._INF)
+
+        self._test_norm(defuzzify.ACC, new_defuzzify.ACC)
+        self._test_norm(defuzzify._ACC, new_defuzzify._ACC)
+
+    def _test_rule(self, rule, new_rule):
+        "test only a given rule"
+
+        self.assertIsInstance(new_rule, rule.__class__)
+
+        self.assertEquals(rule.certainty, new_rule.certainty)
+        print "rule: ", rule.name
+        self._test_adj(rule.adjective, new_rule.adjective)
+        self._test_norm(rule.CER, new_rule.CER)
+        self._test_norm(rule.CER, new_rule.CER)
+        self._test_operator(rule.operator, new_rule.operator)
+
+    def _test_operator(self, operator, new_operator):
+        "test only a given rule"
+        self.assertIsInstance(new_operator, operator.__class__)
+
+        if operator.__class__.__name__ == 'Compound':
+            self._test_norm(operator.norm, new_operator.norm)
+
+            for i_inputs in xrange(0, len(operator.inputs)):
+                inp = operator.inputs[i_inputs]
+                new_inp = new_operator.inputs[i_inputs]
+
+                self._test_operator(inp, new_inp)
+
+        elif operator.__class__.__name__ == 'Const':
+            self.assertEquals(new_operator.value, operator.value)
+
+        elif operator.__class__.__name__ == 'Input':
+
+            print "op: INPUT"
+            self._test_adj(operator.adjective, new_operator.adjective)
+
+        elif operator.__class__.__name__ == 'Not':
+            self._test_operator(operator.input, new_operator.input)
+
+        op_call = operator()
+        new_op_call = new_operator()
+        self.assertEquals(
+            op_call,
+            new_op_call,
+            msg="%s != %s in %s" % (op_call, new_op_call, operator)
+        )
